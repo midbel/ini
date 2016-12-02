8ef9a009f17b66385d29be3b4e56f6a7785a60b8
nico <brun.nicolas21@gmail.com>
2016-12-02 18:08:38 +0100

    example read + TestReadAccount

--- ini_test.go
+++ ini_test.go
@@ -1,6 +1,7 @@
 package ini
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
@@ -62,12 +63,44 @@ size   = 1024
 keep   = false
 `
 
+func ExampleRead() {
+	r := NewReader(strings.NewReader(account))
+	r.Default = "account"
+	
+	a := struct{
+		User string
+		Passwd string
+		Enabled bool
+	}{}
+	
+	r.Read(&a)
+	fmt.Printf("%+v\n", a)
+	//Output:
+	//{User:nobody Passwd:foobar Enabled:true}
+}
+
 func TestReadAccount(t *testing.T) {
 	r := NewReader(strings.NewReader(account))
 	r.Default = "account"
-	if err := r.Read(nil); err != nil {
+	
+	a := struct{
+		User string
+		Passwd string
+		Enabled bool
+	}{"root", "helloworld", false}
+	if err := r.Read(&a); err != nil {
 		t.Errorf("unexpected error: %s", err)
 	}
+	if a.User != "nobody" {
+		t.Errorf("user: expected nobody; got %s", a.User)
+	}
+	if a.Passwd != "foobar" {
+		t.Errorf("passwd: expected nobody; got %s", a.Passwd)
+	}
+	if !a.Enabled {
+		t.Errorf("enabled: expected true; got %s", a.Enabled)
+	}
+	t.Logf("%+v", a)
 }
 
 func TestReadDirectory(t *testing.T) {
